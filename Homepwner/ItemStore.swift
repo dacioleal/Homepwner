891c44f8be9928d97ba8acaf1e2ebe2808044b21
dacioleal <dacioleal@gmail.com>
2016-02-13 12:30:24 +0100

    Added ItemStore class for item collection

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,21 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Dacio Leal Rodriguez on 13/2/16.
+//  Copyright © 2016 Dacio Leal Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    func createItem() -> Item {
+        
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+}
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
+
 class ItemsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let item = Item(random: true)
+        print("Item name:\(item.name) serialNumber:\(item.serialNumber) value:\(item.valueInDollars)")
+        
+        
     }
 
     // MARK: - Table view data source
